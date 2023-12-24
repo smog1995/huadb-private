c56c9d56191872c6a9d2969858de66d23e005b36
smog1995 <ma1653394883@outlook.com>
2023-12-24 23:31:13 +0800

    lab3

--- src/transaction/lock_manager.cpp
+++ src/transaction/lock_manager.cpp
@@ -1,4 +1,6 @@
 #include "transaction/lock_manager.h"
+#include <memory>
+#include <mutex>
 
 namespace huadb {
 
@@ -6,6 +8,37 @@ bool LockManager::LockTable(xid_t xid, LockType lock_type, oid_t oid) {
   // 对数据表加锁，成功加锁返回 true，如果数据表已被其他事务加锁，且锁的类型不相容，返回 false
   // 如果本事务已经持有该数据表的锁，根据需要升级锁的类型
   // LAB 3 BEGIN
+  table_lock_map_latch_.lock();
+  if (table_lock_map_.find(oid) == table_lock_map_.end()) {
+    table_lock_map_.insert(std::make_pair(oid, std::make_shared<LockRequestQueue>()));
+  }
+  auto lock_request_queue = table_lock_map_[oid];
+  table_lock_map_latch_.unlock();
+  std::unique_lock<std::mutex> lock(lock_request_queue->latch_);
+  for (auto ele = lock_request_queue->request_queue_.begin(); ele != lock_request_queue->request_queue_.end(); ele++) {
+    // 相同事务，可能为锁升级
+    if ((*ele)->xid_ == xid) {
+      LockType upgrade_lock_type = Upgrade((*ele)->lock_type_, lock_type);
+      bool compatible = true;
+      for (auto &iter :lock_request_queue->request_queue_) {
+        if (!Compatible(upgrade_lock_type, iter->lock_type_)) {
+          compatible = false;
+          break;
+        }
+      }
+      if (compatible) {
+        (*ele)->lock_type_ = upgrade_lock_type; // 修改为新的锁类型
+        return true;
+      } else {
+        return false;
+      }
+      //  不同事务，判断是否符合锁兼容
+    } else if (!Compatible((*ele)->lock_type_, lock_type)) {
+      return false;
+    }
+  }
+  auto lock_request = std::make_unique(new LockRequest(xid, lock_type, oid));
+  lock_request_queue->request_queue_.push_back(lock_request);
   return true;
 }
 
@@ -13,6 +46,37 @@ bool LockManager::LockRow(xid_t xid, LockType lock_type, oid_t oid, Rid rid) {
   // 对数据行加锁，成功加锁返回 true，如果数据行已被其他事务加锁，且锁的类型不相容，返回 false
   // 如果本事务已经持有该数据行的锁，根据需要升级锁的类型
   // LAB 3 BEGIN
+  row_lock_map_latch_.lock();
+  if (row_lock_map_.find(rid) == row_lock_map_.end()) {
+    row_lock_map_.insert(std::make_pair(rid, std::make_shared<LockRequestQueue>()));
+  }
+  auto lock_request_queue = row_lock_map_[rid];
+  row_lock_map_latch_.unlock();
+  std::unique_lock<std::mutex> lock(lock_request_queue->latch_);
+  for (auto ele = lock_request_queue->request_queue_.begin(); ele != lock_request_queue->request_queue_.end(); ele++) {
+    // 相同事务，可能为锁升级
+    if ((*ele)->xid_ == xid) {
+      LockType upgrade_lock_type = Upgrade((*ele)->lock_type_, lock_type);
+      bool compatible = true;
+      for (auto &iter :lock_request_queue->request_queue_) {
+        if (!Compatible(upgrade_lock_type, iter->lock_type_)) {
+          compatible = false;
+          break;
+        }
+      }
+      if (compatible) {
+        (*ele)->lock_type_ = upgrade_lock_type; // 修改为新的锁类型
+        return true;
+      } else {
+        return false;
+      }
+      //  不同事务，判断是否符合锁兼容
+    } else if (!Compatible((*ele)->lock_type_, lock_type)) {
+      return false;
+    }
+  }
+  auto lock_request = std::make_unique(new LockRequest(xid, lock_type, oid));
+  lock_request_queue->request_queue_.push_back(lock_request);
   return true;
 }
 
@@ -26,13 +90,17 @@ void LockManager::SetDeadLockType(DeadlockType deadlock_type) { deadlock_type_ =
 bool LockManager::Compatible(LockType type_a, LockType type_b) {
   // 判断锁是否相容
   // LAB 3 BEGIN
-  return true;
+  return compatable_lock_[static_cast<int>(type_a)][static_cast<int>(type_b)];
 }
 
 LockType LockManager::Upgrade(LockType self, LockType other) {
   // 升级锁类型
   // LAB 3 BEGIN
-  return LockType::IS;
+  // 能升级则返回升级后的类型，否则返回原类型
+  if (upgrade_lock_[static_cast<int>(self)][static_cast<int>(other)]) {
+    return other;
+  }
+  return self;
 }
 
 }  // namespace huadb
--- src/transaction/lock_manager.h
+++ src/transaction/lock_manager.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <mutex>
+#include <memory>
+#include <unordered_map>
+#include "common/constants.h"
 #include "common/typedefs.h"
+#include "list"
 
 namespace huadb {
 
@@ -19,6 +24,30 @@ enum class DeadlockType { NONE, WAIT_DIE, WOUND_WAIT, DETECTION };
 
 class LockManager {
  public:
+  class LockRequest {
+    public:
+      LockRequest(xid_t xid, LockType lock_type, oid_t oid) /** Table lock request */
+          : xid_(xid), lock_type_(lock_type), oid_(oid) {}
+      LockRequest(xid_t txn_id, LockType lock_type, oid_t oid, Rid rid) /** Row lock request */
+          : xid_(txn_id), lock_type_(lock_type), oid_(oid), rid_(rid) {}
+
+      /** Txn_id of the txn requesting the lock */
+      xid_t xid_;
+      /** Locking mode of the requested lock */
+      LockType lock_type_;
+      /** Oid of the table for a table lock; oid of the table the row belong to for a row lock */
+      oid_t oid_;
+      /** Rid of the row for a row lock; unused for table locks */
+      Rid rid_;
+    };
+  class LockRequestQueue {
+    public:
+    /** List of lock requests for the same resource (table or row) */
+    std::list<std::unique_ptr<LockRequest>> request_queue_;
+    /** coordination */
+    xid_t upgrading_ = DDL_XID;
+    std::mutex latch_;
+  };
   // 获取表级锁
   bool LockTable(xid_t xid, LockType lock_type, oid_t oid);
   // 获取行级锁
@@ -31,11 +60,31 @@ class LockManager {
 
  private:
   // 判断锁的相容性
+  //  LockMode {IS IX S SIX X};
+  bool upgrade_lock_[5][5] = {{true, true, false, false, true},
+                             {false, false, false, false, false},
+                             {true, true, true, true, true},
+                             {false, true, false, true, true},
+                             {false, true, false, false, false}};
+  bool compatable_lock_[5][5] = {{true, true, true, true, false},
+                                 {true, true, false, false, false},
+                                 {true, false, true, false, false},
+                                 {false, false, false, false, false},
+                                 {false, false, false, false, false}};
   bool Compatible(LockType type_a, LockType type_b);
+  
+  
   // 实现锁的升级
   LockType Upgrade(LockType self, LockType other);
 
   DeadlockType deadlock_type_ = DeadlockType::NONE;
+
+  std::mutex row_lock_map_latch_;
+  std::mutex table_lock_map_latch_;
+
+  std::unordered_map<oid_t, std::shared_ptr<LockRequestQueue> > table_lock_map_;
+  
+  std::unordered_map<Rid, std::shared_ptr<LockRequestQueue> > row_lock_map_;
 };
 
 }  // namespace huadb
